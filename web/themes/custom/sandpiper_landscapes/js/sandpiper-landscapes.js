45b94fcb7a9bd848d00a7fa2531f1e66ec47780a
CharlesJGantt <cjgantt@gmail.com>
2025-06-25 21:03:08 -0400

    Updated all the things

---
+++ web/themes/custom/sandpiper_landscapes/js/sandpiper-landscapes.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Sandpiper Landscapes behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.sandpiperLandscapes = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
